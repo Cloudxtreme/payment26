88ac75b64779e7c999d167b6d5534e2beadc0ce0
emmanuelsf <emmanuel.sf@hotmail.com>
2016-06-27 16:21:37 -0300

    Backgorund, Komen Callouts, Dependencies separation, Sidebar

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<?php
+  require 'lib/Stripe.php';
+  //var_dump($_POST);
+  if ($_POST) {
+    Stripe::setApiKey("sk_test_IUz6zrnBkj94Rheo7LpYIKXx");
+    $error = '';
+    $success = '';
+
+    try {
+    if (empty($_POST['street']) || empty($_POST['city']) || empty($_POST['zip']))
+        throw new Exception("Fill out all required fields.");
+      if (!isset($_POST['stripeToken']))
+        throw new Exception("The Stripe Token was not generated correctly");
+      Stripe_Charge::create(array("amount" => 3000,
+                                  "currency" => "eur",
+                                  "card" => $_POST['stripeToken'],
+                  "description" => $_POST['email']));
+      $success = '<div class="alert alert-success">
+                  <strong>Success!</strong> Your payment was successful.
+          </div>';
+    }
+    catch (Exception $e) {
+    $error = '<div class="alert alert-danger">
+          <strong>Error!</strong> '.$e->getMessage().'
+          </div>';
+    }
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -9,228 +37,23 @@
     <link rel="stylesheet" href="css/bootstrapValidator-min.css"/>
     <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" />
     <link rel="stylesheet" href="css/bootstrap-side-notes.css" />
-    <link rel="stylesheet" href="css/cpratt-callouts.css">
-    <style type="text/css">
-      .col-centered {
-          display:inline-block;
-          float:none;
-          text-align:left;
-          margin-right:-4px;
-      }
-      .row-centered {
-      	margin-left: 9px;
-      	margin-right: 9px;
-      }
-    </style>
+    <link rel="stylesheet" href="css/custom-callouts.css">
+    <link rel="stylesheet" type="text/css" href="css/payment.css">
+    
     <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="js/bootstrap-min.js"></script>
     <script src="js/bootstrap-formhelpers-min.js"></script>
     <script type="text/javascript" src="js/bootstrapValidator-min.js"></script>
-    <script type="text/javascript">
-    $(document).ready(function() {
-        $('#payment-form').bootstrapValidator({
-            message: 'This value is not valid',
-            feedbackIcons: {
-                valid: 'glyphicon glyphicon-ok',
-                invalid: 'glyphicon glyphicon-remove',
-                validating: 'glyphicon glyphicon-refresh'
-            },
-    		    submitHandler: function(validator, form, submitButton) {
-              // createToken returns immediately - the supplied callback submits the form if there are no errors
-              Stripe.card.createToken({
-                  number: $('.card-number').val(),
-                  cvc: $('.card-cvc').val(),
-                  exp_month: $('.card-expiry-month').val(),
-                  exp_year: $('.card-expiry-year').val(),
-              		name: $('.card-holder-name').val(),
-              		address_line1: $('.address').val(),
-              		address_city: $('.city').val(),
-              		address_zip: $('.zip').val(),
-              		address_state: $('.state').val(),
-              		address_country: $('.country').val()
-              }, stripeResponseHandler);
-              return false; // submit from callback
-            },
-            fields: {
-              street: {
-                  validators: {
-                      notEmpty: {
-                          message: 'The street is required and cannot be empty'
-                      },
-    				          stringLength: {
-                          min: 6,
-                          max: 96,
-                          message: 'The street must be more than 6 and less than 96 characters long'
-                      }
-                  }
-              },
-              city: {
-                  validators: {
-                      notEmpty: {
-                          message: 'The city is required and cannot be empty'
-                      }
-                  }
-              },
-    	        zip: {
-                  validators: {
-                      notEmpty: {
-                          message: 'The zip is required and cannot be empty'
-                      },
-    			    stringLength: {
-                          min: 3,
-                          max: 9,
-                          message: 'The zip must be more than 3 and less than 9 characters long'
-                      }
-                  }
-              },
-              email: {
-                validators: {
-                      notEmpty: {
-                          message: 'The email address is required and can\'t be empty'
-                      },
-                      emailAddress: {
-                          message: 'The input is not a valid email address'
-                      },
-    				    stringLength: {
-                          min: 6,
-                          max: 65,
-                          message: 'The email must be more than 6 and less than 65 characters long'
-                      }
-                  }
-              },
-    		      cardholdername: {
-                  validators: {
-                      notEmpty: {
-                          message: 'The card holder name is required and can\'t be empty'
-                      },
-    				  stringLength: {
-                          min: 6,
-                          max: 70,
-                          message: 'The card holder name must be more than 6 and less than 70 characters long'
-                      }
-                  }
-              },
-    		      cardnumber: {
-    	          selector: '#cardnumber',
-                validators: {
-                  notEmpty: {
-                    message: 'The credit card number is required and can\'t be empty'
-                  },
-                  creditCard: {
-                    message: 'The credit card number is invalid'
-                  },
-                }
-              },
-    		      expMonth: {
-                selector: '[data-stripe="exp-month"]',
-                validators: {
-                  notEmpty: {
-                      message: 'The expiration month is required'
-                  },
-                  digits: {
-                      message: 'The expiration month can contain digits only'
-                  },
-                  callback: {
-                    message: 'Expired',
-                    callback: function(value, validator) {
-                      value = parseInt(value, 10);
-                      var year         = validator.getFieldElements('expYear').val(),
-                          currentMonth = new Date().getMonth() + 1,
-                          currentYear  = new Date().getFullYear();
-                      if (value < 0 || value > 12) {
-                          return false;
-                      }
-                      if (year == '') {
-                          return true;
-                      }
-                      year = parseInt(year, 10);
-                      if (year > currentYear || (year == currentYear && value > currentMonth)) {
-                          validator.updateStatus('expYear', 'VALID');
-                          return true;
-                      } else {
-                          return false;
-                      }
-                    }
-                  }
-                }
-              },
-              expYear: {
-                selector: '[data-stripe="exp-year"]',
-                validators: {
-                  notEmpty: {
-                      message: 'The expiration year is required'
-                  },
-                  digits: {
-                      message: 'The expiration year can contain digits only'
-                  },
-                  callback: {
-                    message: 'Expired',
-                    callback: function(value, validator) {
-                      value = parseInt(value, 10);
-                      var month        = validator.getFieldElements('expMonth').val(),
-                          currentMonth = new Date().getMonth() + 1,
-                          currentYear  = new Date().getFullYear();
-                      if (value < currentYear || value > currentYear + 100) {
-                          return false;
-                      }
-                      if (month == '') {
-                          return false;
-                      }
-                      month = parseInt(month, 10);
-                      if (value > currentYear || (value == currentYear && month > currentMonth)) {
-                          validator.updateStatus('expMonth', 'VALID');
-                          return true;
-                      } else {
-                          return false;
-                      }
-                    }
-                  }
-                }
-              },
-    		      cvv: {
-    	          selector: '#cvv',
-                validators: {
-                  notEmpty: {
-                    message: 'The cvv is required and can\'t be empty'
-                  },
-    		          cvv: {
-                    message: 'The value is not a valid CVV',
-                    creditCardField: 'cardnumber'
-                  }
-                }
-              },
-            }
-        });
-    });
-    </script>
-    <script type="text/javascript">
-      // this identifies your website in the createToken call below
-      Stripe.setPublishableKey('pk_test_Fn23ogInSLReU7YxlCJc7Hl9');
-
-      function stripeResponseHandler(status, response) {
-          if (response.error) {
-              // re-enable the submit button
-              $('.submit-button').removeAttr("disabled");
-      				// show hidden div
-      				document.getElementById('a_x200').style.display = 'block';
-              // show the errors on the form
-              $(".payment-errors").html(response.error.message);
-          } else {
-              var form$ = $("#payment-form");
-              // token contains id, last4, and card type
-              var token = response['id'];
-              // insert the token into the form so it gets submitted to the server
-              form$.append("<input type='hidden' name='stripeToken' value='" + token + "' />");
-              // and submit
-              form$.get(0).submit();
-          }
-      }
-    </script>
+    <script type="text/javascript" src="js/payment.js"></script>
   </head>
   <body>
     <div class="row">
       <div class="col-md-4 col-lg-4 col-xs-12">
+        <div class="sidebar_donate">
+          <p><strong>DONATE BY PHONE</strong></p>
+          <p>1-877 GO KOMEN <br>(1-877-465-6636, option 4)</p>
+        </div>
       </div>
       <div class="col-md-7  col-lg-7 col-xs-12">
         <div class="page-header">
@@ -242,34 +65,6 @@
             <p>This payment form requires your browser to have JavaScript enabled. Please activate JavaScript and reload this page. Check <a href="http://enable-javascript.com" target="_blank">enable-javascript.com</a> for more informations.</p>
           </div>
         </noscript>
-        <?php
-          require 'lib/Stripe.php';
-          //var_dump($_POST);
-          if ($_POST) {
-            Stripe::setApiKey("sk_test_IUz6zrnBkj94Rheo7LpYIKXx");
-            $error = '';
-            $success = '';
-
-            try {
-            if (empty($_POST['street']) || empty($_POST['city']) || empty($_POST['zip']))
-                throw new Exception("Fill out all required fields.");
-              if (!isset($_POST['stripeToken']))
-                throw new Exception("The Stripe Token was not generated correctly");
-              Stripe_Charge::create(array("amount" => 3000,
-                                          "currency" => "eur",
-                                          "card" => $_POST['stripeToken'],
-                          "description" => $_POST['email']));
-              $success = '<div class="alert alert-success">
-                          <strong>Success!</strong> Your payment was successful.
-                  </div>';
-            }
-            catch (Exception $e) {
-            $error = '<div class="alert alert-danger">
-                  <strong>Error!</strong> '.$e->getMessage().'
-                  </div>';
-            }
-          }
-        ?>
         <div class="alert alert-danger" id="a_x200" style="display: none;"> <strong>Error!</strong> <span class="payment-errors"></span> </div>
         <?php if (isset($success) || isset($error)): ?>
           <span class="payment-success">
@@ -278,7 +73,7 @@
           </span>
         <?php endif; ?>
         <form action="" method="POST" id="payment-form" class="form-horizontal">
-          <div class="bs-callout bs-callout-info">
+          <div class="komen-callout">
             <fieldset>      
               <!-- Form Name -->
               <legend>Billing Details</legend>
@@ -333,10 +128,11 @@
               </div>
             </fieldset>
           </div>
-          <div class="bs-callout bs-callout-info">
+          <div class="komen-callout">
             <fieldset>
-              <legend>Card Details</legend>
-              
+              <legend>Payment Information</legend>
+              <p>All transactions conducted on this website are encrypted using a secure server to protect your privacy. View Privacy Policy.</p>
+              <div class="payamount"><p class="amtmonies">$0.00</p><p class="amttimes">ONE-TIME DONATION</p></div>
               <!-- Card Holder Name -->
               <div class="form-group">
                 <label class="col-sm-4 control-label"  for="textinput">Card Holder's Name</label>
@@ -375,14 +171,6 @@
                     <span> / </span>
                     <select name="select2" data-stripe="exp-year" class="card-expiry-year stripe-sensitive required form-control">
                     </select>
-                    <script type="text/javascript">
-                      var select = $(".card-expiry-year"),
-                      year = new Date().getFullYear();
-           
-                      for (var i = 0; i < 12; i++) {
-                          select.append($("<option value='"+(i + year)+"' "+(i === 0 ? "selected" : "")+">"+(i + year)+"</option>"))
-                      }
-                  </script> 
                   </div>
                 </div>
               </div>
@@ -395,7 +183,7 @@
               </div>          
             </fieldset>
           </div>
-          <div class="bs-callout bs-callout-info">
+          <div class="komen-callout">
             <fieldset>            
               <!-- Important notice -->
               <div class="form-group">
