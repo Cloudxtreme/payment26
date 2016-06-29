315049b6f94906feeed76504f3155b2e92d02333
agustin-mm <matuagustin@hotmail.com>
2016-06-29 16:24:13 -0300

    popovers
    mostrar información extra al hacer click sobre el span info

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Secure Payment Form</title>
-    
+
     <link href="//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700" rel="stylesheet" type="text/css">
     <link href="//fonts.googleapis.com/css?family=Raleway:400,100,300,500,600" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="css/bootstrap-min.css">
@@ -65,6 +65,10 @@
               <p>This payment form requires your browser to have JavaScript enabled. Please activate JavaScript and reload this page. Check <a href="http://enable-javascript.com" target="_blank">enable-javascript.com</a> for more informations.</p>
             </div>
           </noscript>
+          <script>
+          $(function () {$('[data-toggle="popover"]').popover({html:true,placement:"bottom"})})/*inicializa popover*/
+
+          </script>
           <div class="alert alert-danger" id="a_x200" style="display: none;"> <strong>Error!</strong> <span class="payment-errors"></span> </div>
           <?php if (isset($success) || isset($error)): ?>
             <span class="payment-success">
@@ -83,7 +87,7 @@
               <div class="row">
                 <div class="komen-callout komen-section col-xs-12 col-sm-12 col-md-12 col-lg-12">
                   <h2><span class="glyphicon glyphicon-asterisk required"></span>Donation Amount</h2>
-                  <div class="wrapper-price">
+                  <div class="row">
                     <input type="radio" name="donation-level" id="donation-level-1" value="25">
                     <label for="donation-level-1">$25</label>
                     <input type="radio" name="donation-level" id="donation-level-2" value="50">
@@ -96,23 +100,24 @@
                     <label for="donation-level-5">$500</label>
                     <input type="radio" name="donation-level" id="donation-level-6" value="0">
                     <label for="donation-level-6">OTHER</label>
-                    <div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                       <input class="form-control" type="text" name="" id="" placeholder="$0.00">
                     </div>
                   </div>
                   <div class="checkbox">
                     <input type="checkbox" name="" id="level_standardauto_repeatname">
                     <label for="level_standardauto_repeatname"> Make this my monthly donation.</label>
-                    <span class="glyphicon glyphicon-info-sign info"></span>
+                    <span data-trigger="click" tabindex="0" class="glyphicon glyphicon-info-sign info" role="button" data-toggle="popover" data-content="If selected, you will be enrolled in our monthly program. Your monthly gift will be processed automatically with the credit card information you have provided.<br><a href='http://ww5.komen.org/HelpFAQ/HelpFAQs.html' target='_blank'>Learn More</a>"></span>
                   </div>
                   <div class="checkbox">
                     <input type="checkbox" name="" id="tribute_show_honor_fieldsname">
                     <label for="tribute_show_honor_fieldsname">Make my gift in honor or memory of a friend or loved one.</label>
-                    <span class="glyphicon glyphicon-info-sign info"></span>
+                    <span data-trigger="click" tabindex="0" class="glyphicon glyphicon-info-sign info" role="button" data-toggle="popover" data-content="<strong>Honorariums</strong> are a great way to show your appreciation for a special someone by making a donation to Susan G. Komen® in their name. Honor gifts are perfect for anniversaries, birthdays and other special events.<br>
+                    <strong>A Memorial gift</strong> is a beautiful tribute to the life of a loved one who has passed on. "></span>
                   </div>
 
                   <div id="tribute_honoree_last_name_row">
-                    <div class="form-content">
+                    <div class="form-content col-xs-12 col-sm-12 col-md-12 col-lg-12">
                       <span class="glyphicon glyphicon-asterisk required"></span>
                       <label for="tribute_honoree_last_namename">Person's Name or Occasion</label>
                       <input class="form-control" name="tribute_honoree_last_namename" id="tribute_honoree_last_namename" value="" maxlength="50" type="text" placeholder="Person's Name or Occasion">
@@ -144,22 +149,28 @@
                   <div class="radio">
                     <input type="radio" name="fund_radio" id="donation-stageIV">
                     <label for="donation-stageIV">Stage IV/ Metastatic Breast Cancer Research.</label>
-                    <span class="glyphicon glyphicon-info-sign info"></span>
+                    <span data-trigger="click" tabindex="0" class="glyphicon glyphicon-info-sign info" role="button" data-toggle="popover" data-content=" <strong>Stage IV/ Metastatic Breast Cancer Research</strong>
+                    Your gift will help us support research that focuses on Stage IV/ Metastatic Breast Cancer Research—cancer that has spread beyond the breast to other organs which is responsible for nearly all breast cancer deaths (including 40,000 in the U.S. this year alone)."></span>
+
                   </div>
                   <div class="radio">
                     <input type="radio" name="fund_radio" id="donation-generalBC">
                     <label for="generalBC">General Breast Cancer Research.</label>
-                    <span class="glyphicon glyphicon-info-sign info"></span>
+                    <span data-trigger="click" tabindex="0" class="glyphicon glyphicon-info-sign info" role="button" data-toggle="popover" data-content=" <strong>General Breast Cancer Research</strong>
+                    Your gift will help us support the best breast cancer research, investigating everything from prevention through treatment and beyond."></span>
                   </div>
                   <div class="radio">
                     <input type="radio" name="fund_radio" id="donation-">
                     <label for="donation-tripleNBC">Triple Negative Breast Cancer Research.</label>
-                    <span class="glyphicon glyphicon-info-sign info"></span>
+                    <span data-trigger="click" tabindex="0" class="glyphicon glyphicon-info-sign info" role="button" data-toggle="popover" data-content="<strong>Triple Negative Breast Cancer Research</strong>
+                    Your gift will help us support research focusing on triple negative breast cancer a highly aggressive form of breast cancer for which no targeted treatment currently exists."></span>
                   </div>
                   <div class="radio">
                     <input type="radio" name="fund_radio" id="donation-generalFund">
                     <label for="generalFund">General Fund.</label>
-                    <span class="glyphicon glyphicon-info-sign info"></span>
+                    <span data-trigger="click" tabindex="0" class="glyphicon glyphicon-info-sign info" role="button" data-toggle="popover" data-content="<strong> General Fund</strong>
+                    Your gift will help us support research, education and support while providing information and other programs to assist women, men and families facing breast cancer, and general administrative costs."></span>
+
                   </div>
                 </div>
               </div>
@@ -176,13 +187,13 @@
                         <div class="form-group card-data">
                           <label class="control-label" for="textinput"><span class="glyphicon glyphicon-asterisk required"></span>Credit Card Number:</label>
                           <input type="text" id="cardnumber" maxlength="19" placeholder="Card Number" class="card-number form-control">
-                        </div>                        
+                        </div>
                       </div>
                       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3">
                         <!-- CVV -->
                         <div class="form-group card-data">
                           <label class="control-label" for="textinput"><span class="glyphicon glyphicon-asterisk required"></span>CVV:</label>
-                            <input type="text" id="cvv" placeholder="CVV" maxlength="4" class="card-cvc form-control">                          
+                            <input type="text" id="cvv" placeholder="CVV" maxlength="4" class="card-cvc form-control">
                         </div>
                       </div>
                       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
@@ -207,8 +218,8 @@
                             <span> / </span>
                             <select name="select2" data-stripe="exp-year" class="card-expiry-year stripe-sensitive required form-control">
                             </select>
-                          </div>                          
-                        </div>                        
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </fieldset>
@@ -235,12 +246,12 @@
                         <!-- Phone Number -->
                         <div class="form-group">
                           <label class="control-label" for="textinput"><span class="glyphicon glyphicon-asterisk required"></span>Phone Number</label>
-                          <input type="text" id="phone" name="phone" maxlength="30" placeholder="Phone Number" class="form-control">                        
+                          <input type="text" id="phone" name="phone" maxlength="30" placeholder="Phone Number" class="form-control">
                         </div>
                         <!-- Email -->
                         <div class="form-group">
                           <label class="control-label" for="textinput"><span class="glyphicon glyphicon-asterisk required"></span>Email Address</label>
-                          <input type="text" name="email" maxlength="65" placeholder="Email" class="email form-control">                        
+                          <input type="text" name="email" maxlength="65" placeholder="Email" class="email form-control">
                         </div>
                       </div>
                     </div>
@@ -256,7 +267,7 @@
                       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <!-- Country -->
                         <div class="form-group">
-                          <label class="control-label" for="textinput"><span class="glyphicon glyphicon-asterisk required"></span>Country</label>                          
+                          <label class="control-label" for="textinput"><span class="glyphicon glyphicon-asterisk required"></span>Country</label>
                           <!--input type="text" name="country" placeholder="Country" class="country form-control"-->
                           <div class="country bfh-selectbox bfh-countries" name="country" placeholder="Select Country" data-flags="true" data-filter="true"></div>
                         </div>
@@ -267,14 +278,14 @@
                         <!-- Postcal Code -->
                         <div class="form-group">
                           <label class="control-label" for="textinput"><span class="glyphicon glyphicon-asterisk required"></span>Postal Code</label>
-                          <input type="text" name="zip" maxlength="9" placeholder="Postal Code" class="zip form-control">                          
+                          <input type="text" name="zip" maxlength="9" placeholder="Postal Code" class="zip form-control">
                         </div>
                       </div>
                       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
                         <!-- Street -->
                         <div class="form-group">
-                          <label class="control-label" for="textinput"><span class="glyphicon glyphicon-asterisk required"></span>Street</label>                          
-                          <input type="text" name="street" placeholder="Street" class="address form-control">                          
+                          <label class="control-label" for="textinput"><span class="glyphicon glyphicon-asterisk required"></span>Street</label>
+                          <input type="text" name="street" placeholder="Street" class="address form-control">
                         </div>
                       </div>
                     </div>
@@ -282,15 +293,15 @@
                       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
                         <!-- City -->
                         <div class="form-group">
-                          <label class="control-label" for="textinput"><span class="glyphicon glyphicon-asterisk required"></span>City</label>                          
-                            <input type="text" name="city" placeholder="City" class="city form-control">                          
+                          <label class="control-label" for="textinput"><span class="glyphicon glyphicon-asterisk required"></span>City</label>
+                            <input type="text" name="city" placeholder="City" class="city form-control">
                         </div>
                       </div>
                       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
                         <!-- State -->
                         <div class="form-group">
-                          <label class="control-label" for="textinput"><span class="glyphicon glyphicon-asterisk required"></span>State</label>                          
-                          <input type="text" name="state" maxlength="65" placeholder="State" class="state form-control">                          
+                          <label class="control-label" for="textinput"><span class="glyphicon glyphicon-asterisk required"></span>State</label>
+                          <input type="text" name="state" maxlength="65" placeholder="State" class="state form-control">
                         </div>
                       </div>
                     </div>
